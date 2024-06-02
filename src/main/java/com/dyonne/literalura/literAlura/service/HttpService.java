c8eb6e53605988c1c865c9b0eb9b85f73006b0a6
dyonne <bydyonne@gmail.com>
2024-06-02 13:37:13 -0300

    Construction of the class responsible for consuming data from the api

---
+++ src/main/java/com/dyonne/literalura/literAlura/service/HttpService.java
@@ -0,0 +1,28 @@
+package com.dyonne.literalura.literAlura.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HttpService {
+
+    public String getApiData (String url) {
+        HttpClient client = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.ALWAYS)
+                .build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return response.body();
+    }
+}
