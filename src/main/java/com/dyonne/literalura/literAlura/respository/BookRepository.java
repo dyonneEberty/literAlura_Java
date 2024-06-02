cb217e902483e7cfa3efd094f328452916e8381e
dyonne <bydyonne@gmail.com>
2024-06-02 15:53:16 -0300

    Construct of the BOOK repository resposible for querying the database

---
+++ src/main/java/com/dyonne/literalura/literAlura/respository/BookRepository.java
@@ -0,0 +1,17 @@
+package com.dyonne.literalura.literAlura.respository;
+
+import com.dyonne.literalura.literAlura.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Book getByTitle(String title);
+
+    @Query("SELECT DISTINCT b.language FROM Book b ORDER BY b.language")
+    List<String> languages();
+    @Query("SELECT b FROM Book b WHERE language = :language")
+    List<Book> booksByLanguage(String language);
+}
