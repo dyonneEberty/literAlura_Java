f97009cb4cbc50f5a4e7392c30a4066ad02d4274
dyonne <bydyonne@gmail.com>
2024-06-02 13:26:10 -0300

    Construction of the author class for data persistence in the database

--- src/main/java/com/dyonne/literalura/literAlura/model/Author.java
+++ src/main/java/com/dyonne/literalura/literAlura/model/Author.java
@@ -1,4 +1,52 @@
 package com.dyonne.literalura.literAlura.model;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+    private Integer birth_year;
+    private Integer death_year;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Book> books = new ArrayList<>();
+
+    public Author() {}
+    public Author(AuthorData authorData) {
+        String[] author = authorData.name().split(",");
+        this.name = author[1] + " " + author[0];
+        this.birth_year = authorData.birth_year();
+        this.death_year = authorData.death_year();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getBirth_year() {
+        return birth_year;
+    }
+
+    public Integer getDeath_year() {
+        return death_year;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Book book) {
+        this.books.add(book);
+        book.setAuthor(this);
+    }
 }
